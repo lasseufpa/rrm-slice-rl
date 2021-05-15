6349c61427910754fd4be75fa5d012679e103fa6
Cleverson Nahum <cleversonahum@gmail.com>
2021-05-14 21:08:28 -0300

    Changing dropped and sent packets count
    I've changed the logic of cumulative dropped and sent packets to a return
    of number of dropped and sent packets per step.

--- buffer.py
+++ buffer.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class Buffer:
-    dropped_packets = 0  # Accumulate the number of dropped packets
+    dropped_packets = 0  # number of dropped packets per step
+    sent_packets = 0  # number of sent packets per step
 
     def __init__(self, max_packets_buffer: int, max_packet_age: int) -> None:
         self.buffer = np.zeros(max_packet_age)
@@ -10,7 +11,7 @@ class Buffer:
         self.max_packets_buffer = max_packets_buffer
         self.max_packets_age = max_packet_age
 
-    def arrived_packets(self, num_packets_arrived: int) -> None:
+    def receive_packets(self, num_packets_arrived: int) -> None:
         """
         Add the arrived packets to the buffer structure. The buffer is represented
         by a 1-D array, where the index 0 represents the packets that arrived now
@@ -19,6 +20,7 @@ class Buffer:
         steps or when the num_packets_arrived is greater than the buffer space
         available (depends on max_packets_buffer).
         """
+        self.dropped_packets = 0
         self.dropped_packets += self.buffer[-1]
         self.buffer = np.roll(self.buffer, 1)
         self.buffer[0] = 0
@@ -30,7 +32,7 @@ class Buffer:
             )
             self.buffer[0] = self.max_packets_buffer - np.sum(self.buffer)
 
-    def sent_packets(self, packets_available_to_sent: int) -> None:
+    def send_packets(self, packets_available_to_sent: int) -> None:
         """
         Transmit packets from buffer to free buffer space. It allocates the packets
         waiting longer time (near from last array element) first.
@@ -45,6 +47,7 @@ class Buffer:
                     self.buffer[i] -= packets_available_to_sent
                     break
         self.cumulative_buffer += tmp_buffer - self.buffer
+        self.sent_packets = np.sum(tmp_buffer) - np.sum(self.buffer)
 
     def get_buffer_occupancy(self) -> np.array:
         """
@@ -72,13 +75,15 @@ def main():
     sent_packets = 2
     ue_buffer = Buffer(20, 5)
     for packets in arrived_packets:
-        ue_buffer.arrived_packets(packets)
+        ue_buffer.receive_packets(packets)
         print(
             ue_buffer.buffer,
             ",  Number of dropped packets: ",
             ue_buffer.dropped_packets,
+            ", Number of sent packets: ",
+            ue_buffer.sent_packets,
         )
-        ue_buffer.sent_packets(sent_packets)
+        ue_buffer.send_packets(sent_packets)
         print(ue_buffer.buffer, "\n")
 
     print(ue_buffer.cumulative_buffer)
