48241fcb65e978dac8fccdc0881fcf3b37ead709
Cleverson Nahum <cleversonahum@gmail.com>
2021-05-14 21:11:50 -0300

    removing unused function

--- buffer.py
+++ buffer.py
@@ -65,9 +65,6 @@ class Buffer:
             (self.cumulative_buffer) * np.arange(1, self.max_packets_age + 1)
         ) / np.sum(self.cumulative_buffer)
 
-    def get_sent_packets(self) -> np.array:
-        return np.sum(self.cumulative_buffer)
-
 
 def main():
     # Testing Buffer functions
