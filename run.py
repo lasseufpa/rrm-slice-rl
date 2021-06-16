009f1f5a7a72aba63a259e71965275ce3c71d088
Cleverson Nahum <cleversonahum@gmail.com>
2021-06-16 14:43:59 -0300

    Run file added as example of how to execute stable baselines with the environment

---
+++ run.py
@@ -0,0 +1,40 @@
+import numpy as np
+from stable_baselines import DQN
+from stable_baselines.common.env_checker import check_env
+from stable_baselines.common.evaluation import evaluate_policy
+
+from basestation import Basestation
+
+# Create environment
+traffic_types = np.concatenate(
+    (np.repeat("embb", 4), np.repeat("urllc", 3), np.repeat("be", 3)), axis=None
+)
+env = Basestation(
+    10 * 65535 * 8, 100, 5000000, 65535 * 8, 10, 1, 17, 2000, traffic_types
+)
+# check_env(env)
+# exit()
+
+# Instantiate the agent
+model = DQN("MlpPolicy", env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
+# Train the agent
+model.learn(total_timesteps=int(100))
+# Save the agent
+model.save("dqn_rrm")
+del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+env = Basestation(
+    10 * 65535 * 8, 100, 5000000, 65535 * 8, 10, 1, 17, 2000, traffic_types
+)
+model = DQN.load("dqn_rrm", env)
+
+# Evaluate the agent
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=2)
+
+# Enjoy trained agent
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
