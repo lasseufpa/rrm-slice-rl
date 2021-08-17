fixing windows average calculation

basestation.py
@@ -38,6 +38,7 @@ class Basestation(gym.Env):
         total_number_rbs: int = 17,
         max_number_steps: int = 2000,
         max_number_trials: int = 50,
+        windows_size: int = 100,
         seed: int = -1,
         plots: bool = False,
     ) -> None:
@@ -57,6 +58,7 @@ class Basestation(gym.Env):
         self.reward = 0
         self.traffic_throughputs = traffic_throughputs
         self.slice_requirements = slice_requirements
+        self.windows_size = windows_size
         self.seed = seed
         self.plots = plots
 
@@ -160,7 +162,7 @@ class Basestation(gym.Env):
                     traffic_throughput=self.traffic_throughputs[i - 1],
                     plots=True,
                     seed=-1 if self.seed == -1 else self.seed + self.trial_number,
-                    windows_size=1,
+                    windows_size=self.windows_size,
                 )
                 for i in np.arange(1, self.number_ues + 1)
             ]

run.py
@@ -104,31 +104,32 @@ models = ["a2c"]  # , "ppo", "dqn"]
 traffics_list = traffics.keys()
 
 # Training
-for model in models:
-    agent = create_agent(model, "train")
-    for traffic_behavior in traffics_list:
-        for run_number in range(1, train_param["runs_per_agent"] + 1):
-            env = Basestation(
-                bs_name="{}_train/{}/run{}".format(model, traffic_behavior, run_number),
-                max_number_steps=train_param["steps_per_trial"],
-                max_number_trials=train_param["total_trials"],
-                traffic_types=traffic_types,
-                traffic_throughputs=traffics[traffic_behavior],
-                slice_requirements=slice_requirements[traffic_behavior],
-                seed=(run_number - 1) * test_param["total_trials"],
-                plots=True,
-            )
-            agent.set_env(env)
-            agent.learn(
-                total_timesteps=int(
-                    train_param["total_trials"] * train_param["steps_per_trial"]
-                ),
-            )
-    agent.save("./agents/{}".format(model))
+# for model in models:
+#     agent = create_agent(model, "train")
+#     for traffic_behavior in traffics_list:
+#         for run_number in range(1, train_param["runs_per_agent"] + 1):
+#             env = Basestation(
+#                 bs_name="{}_train/{}/run{}".format(model, traffic_behavior, run_number),
+#                 max_number_steps=train_param["steps_per_trial"],
+#                 max_number_trials=train_param["total_trials"],
+#                 traffic_types=traffic_types,
+#                 traffic_throughputs=traffics[traffic_behavior],
+#                 slice_requirements=slice_requirements[traffic_behavior],
+#                 windows_size = 100,
+#                 seed=(run_number - 1) * test_param["total_trials"],
+#                 plots=True,
+#             )
+#             agent.set_env(env)
+#             agent.learn(
+#                 total_timesteps=int(
+#                     train_param["total_trials"] * train_param["steps_per_trial"]
+#                 ),
+#             )
+#     agent.save("./agents/{}".format(model))
 
 # Test
-models_test = np.append(models, ["mt", "rr", "pf"])
-# models_test = ["pf"]  # , "rr", "mt"]
+# models_test = np.append(models, ["mt", "rr", "pf"])
+models_test = ["rr"]  # , "rr", "mt"]
 for model in models_test:
     agent = create_agent(model, "test")
     for traffic_behavior in traffics_list:
@@ -140,6 +141,7 @@ for model in models_test:
                 traffic_types=traffic_types,
                 traffic_throughputs=traffics[traffic_behavior],
                 slice_requirements=slice_requirements[traffic_behavior],
+                windows_size=100,
                 seed=(run_number - 1) * test_param["total_trials"],
                 plots=True,
             )

ue.py
@@ -58,6 +58,7 @@ class UE:
             "se",
         ]
         self.hist = {hist_label: np.array([]) for hist_label in self.hist_labels}
+        self.tmp_hist = {hist_label: np.array([]) for hist_label in self.hist_labels}
         self.rng = (
             np.random.default_rng(seed) if seed != -1 else np.random.default_rng()
         )
@@ -141,7 +142,22 @@ class UE:
     ) -> None:
         """
         Update the variables history to enable the record to external files.
+        It creates a tmp_hist that records the history of UE variables without
+        using a windows calculation and it is used as basis to calculate the
+        hist variable using windows average.
         """
+        hist_vars = [
+            packets_received,
+            packets_sent,
+            packets_throughput,
+            buffer_occupancy,
+            avg_latency,
+            pkt_loss,
+            self.se[step_number],
+        ]
+        for i, var in enumerate(self.tmp_hist.items()):
+            self.tmp_hist[var[0]] = np.append(self.tmp_hist[var[0]], hist_vars[i])
+
         slice_idx2 = None
         if step_number < self.windows_size:
             slice_idx1 = 0
@@ -151,34 +167,40 @@ class UE:
                 slice_idx2 = 0
 
         pkt_loss_den = np.sum(
-            np.append(self.hist["pkt_rcv"][slice_idx1:slice_idx2], packets_received)
-        ) + np.sum(self.buffer.buffer)
+            np.append(self.tmp_hist["pkt_rcv"][slice_idx1:slice_idx2], packets_received)
+        )
         hist_vars = [
             np.mean(
-                np.append(self.hist["pkt_rcv"][slice_idx1:slice_idx2], packets_received)
+                np.append(
+                    self.tmp_hist["pkt_rcv"][slice_idx1:slice_idx2], packets_received
+                )
             ),
             np.mean(
-                np.append(self.hist["pkt_snt"][slice_idx1:slice_idx2], packets_sent)
+                np.append(self.tmp_hist["pkt_snt"][slice_idx1:slice_idx2], packets_sent)
             ),
             np.mean(
                 np.append(
-                    self.hist["pkt_thr"][slice_idx1:slice_idx2], packets_throughput
+                    self.tmp_hist["pkt_thr"][slice_idx1:slice_idx2], packets_throughput
                 )
             ),
             np.mean(
                 np.append(
-                    self.hist["buffer_occ"][slice_idx1:slice_idx2], buffer_occupancy
+                    self.tmp_hist["buffer_occ"][slice_idx1:slice_idx2], buffer_occupancy
                 )
             ),
             np.mean(
-                np.append(self.hist["avg_lat"][slice_idx1:slice_idx2], avg_latency)
+                np.append(self.tmp_hist["avg_lat"][slice_idx1:slice_idx2], avg_latency)
             ),
-            np.sum(np.append(self.hist["pkt_loss"][slice_idx1:slice_idx2], pkt_loss))
+            np.sum(
+                np.append(self.tmp_hist["pkt_loss"][slice_idx1:slice_idx2], pkt_loss)
+            )
             / pkt_loss_den
             if pkt_loss_den != 0
             else 0,
             np.mean(
-                np.append(self.hist["se"][slice_idx1:slice_idx2], self.se[step_number])
+                np.append(
+                    self.tmp_hist["se"][slice_idx1:slice_idx2], self.se[step_number]
+                )
             ),
         ]
         for i, var in enumerate(self.hist.items()):